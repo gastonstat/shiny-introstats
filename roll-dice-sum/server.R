b91f6633e072c9270eff9765c4a07a42a81262aa
gastonstat <gaston.stat@gmail.com>
2016-03-20 10:41:02 -0600

    update server in rool-dice-sum app

--- roll-dice-sum/server.R
+++ roll-dice-sum/server.R
@@ -8,12 +8,7 @@ shinyServer(function(input, output) {
   # Fill in the spot we created for a plot
   output$chancePlot <- renderPlot({
     set.seed(input$seed)
-    #
-    if (input$choice == 1){
-      total_points <- sapply(rep(input$dice, input$reps), sum_rolls)
-    } else {
-      total_points <- sapply(rep(input$dice, input$reps), prod_rolls)
-    }
+    total_points <- sapply(rep(input$dice, input$reps), sum_rolls)
     # put in relative terms
     prop_points <- 100 * table(total_points) / input$reps
     ymax <- find_ymax(max(prop_points), 5)

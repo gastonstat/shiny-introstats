a4322a016d597bffde0a524f58caebcfb96e83d4
gastonstat <gaston.stat@gmail.com>
2016-04-01 17:14:19 -0700

    preappended ch18 to roll-dice-sum app

---
+++ ch18-roll-dice-sum/server.R
@@ -0,0 +1,24 @@
+# Sum of dots when rolling a pair of dice
+# Chapter 18: Normal Approximation for Probability Histograms
+# Pages 310-311
+
+library(shiny)
+
+source('helpers.R')
+
+# Define a server for the Shiny app
+shinyServer(function(input, output) {
+  
+  # Fill in the spot we created for a plot
+  output$chancePlot <- renderPlot({
+    set.seed(input$seed)
+    total_points <- sapply(rep(input$dice, input$reps), sum_rolls)
+    # put in relative terms
+    prop_points <- 100 * table(total_points) / input$reps
+    ymax <- find_ymax(max(prop_points), 2)
+    # Render a barplot
+    barplot(prop_points, las = 1, border = "gray40",
+            space = 0, ylim = c(0, ymax),
+            main = sprintf("%s Repetitions", input$reps))
+  })
+})

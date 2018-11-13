d2368db3f72cc756d2ceaa2b0c568abe9f7592d4
Gaston Sanchez <gaston.stat@gmail.com>
2018-11-12 17:59:53 -0800

    ch23-average-number: update fall 2018

--- ch23-average-number/server.R
+++ ch23-average-number/server.R
@@ -65,13 +65,14 @@ shinyServer(function(input, output) {
     
     # Render plot
     samples <- sum_draws() / input$draws
-    a <- samples - se_avg
-    b <- samples + se_avg
+    a <- samples - input$ses * se_avg
+    b <- samples + input$ses * se_avg
     covers <- (a <= avg_box & avg_box <= b)
     ci_cols <- rep('#ff000088', input$reps)
     ci_cols[covers] <- '#0000ff88'
     
-    xlim <- c(min(samples) - se_avg, max(samples) + se_avg)
+    xlim <- c(min(samples) - input$ses * se_avg, 
+              max(samples) + input$ses * se_avg)
     plot(samples, 1:length(samples), axes = FALSE,
          col = '#444444', pch = 21, cex = 0.5,
          xlim = c(2.5, 5.5), ylab = 'Number of samples',
@@ -80,8 +81,8 @@ shinyServer(function(input, output) {
     axis(side = 1)
     axis(side = 2, las = 1)
     abline(v = avg_box, col = '#0000FFdd', lwd = 2.5)
-    segments(x0 = samples - se_avg,
-             x1 = samples + se_avg,
+    segments(x0 = a,
+             x1 = b,
              y0 = 1:length(samples),
              y1 = 1:length(samples),#col = "#555555bb"
              col = ci_cols)
